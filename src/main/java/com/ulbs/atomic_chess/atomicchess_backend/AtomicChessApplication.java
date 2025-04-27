79000df70cc2748b53402eb40cf5b030aec6e58b
andreidumitrel <152534347+andreidumitrel@users.noreply.github.com>
2025-04-27 15:19:20 +0300

    First commit

---
+++ src/main/java/com/ulbs/atomic_chess/atomicchess_backend/AtomicChessApplication.java
@@ -0,0 +1,13 @@
+package com.ulbs.atomic_chess.atomicchess_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicChessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtomicChessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ulbs/atomic_chess/atomicchess_backend/AtomicChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulbs.atomic_chess.atomicchess_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomicChessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
